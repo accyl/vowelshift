ae7851ba66345f2ac7c282314945043e15413470
phlaxyr <phlaxyzerobane@gmail.com>
2021-02-22 23:19:59 -0600

    use classes

--- vowel.js
+++ vowel.js
@@ -1,4 +1,81 @@
-function init() {
+class IPACharset {
+  constructor() {}
+  toChar(closedness, frontedness, rounded) {}
+  charToIdx(ipachar) {}
+}
+class IPACharsetBasic extends IPACharset {
+  constructor() {
+    super();
+    this.rfncstr = 'iy..ɨʉ..ɯu..ɪʏ..ʊʊ..eø..ɘɵ..ɤo....əə....ɛœ..ɜɞ..ʌɔææ..ɐɐ....aɶ......ɑɒ';
+  }
+  idxsToInt(closedness, frontedness, rounded) {
+    if(rounded === 0.5) {
+      rounded = 0; // correct for 0.5 for both rounded/unrounded
+    }
+    var openidx = 6 - closedness * 2;
+    // turn the 0.5s into integers. Range: 0-3 -> 0-6 Len: 7
+    var backidx = 4 - frontedness * 2;
+    // turn the 0.5s into integers. Range: 0-2 -> 0-4 Len: 5
+    // Rounded is already ok.       Range: 0-1        Len: 2
+    var searchidx = rounded + backidx * 2 + openidx * 2 * 5;
+    // 2 comes from len(rounded);
+    // 10 comes from len(rounded x fronted) where x is cartesian product
+    // or 10 comes from len(rounded) * len(fronted) = 2 * 5
+    return searchidx;
+  }
+  toChar(closedness, frontedness, rounded) {
+    var searchidx = idxsToInt(closedness, frontedness, rounded);
+    var ipachar = this.rfncstr.charAt(searchidx);
+    if(ipachar == '.') {
+      // i use this value to signify no result
+      console.log(`invalid char ${closedness},${frontedness},${rounded}`)
+    } else {
+      return ipachar;
+    }
+  }
+  charToIdx(ipachar) {
+    var idxdouble = this.rfncstr.indexOf(ipachar + '' + ipachar);
+    // index of double char. This means that this char spans both
+    // the rounded and unrounded versions of itself. For example, ʊ shows up as
+    // ɪʏʊʊe because ʊ is both the rounded and unrounded version. If this is the
+    // case roundedness must be 0.5;
+    var rounded = -1;
+    if(idxdouble > -1) { // if there is a double version: if both r and unr
+      rounded = 0.5;
+    }
+    var idx = rfncstr.indexOf(ipachar);
+    if(idx > -1) {
+      if(rounded === -1) {
+        var rounded = idx % 2;
+      }
+      var backidx = parseInt(idx/2) % 5;
+      var openidx = parseInt(idx/10);
+      var closedness = (6 - openidx) / 2;
+      var frontedness = (4 - backidx) / 2;
+      return [closedness, frontedness, rounded];
+    } else {
+      console.log(`char not found ${char}`);
+    }
+  }
+}
+class IPACharsetAdvanced extends IPACharsetBasic{
+  constructor() {
+    super();
+    this.rfncstr =    'iy..ɨʉ..ɯu..ɪʏ..ʊʊ..eø..ɘɵ..ɤoeø..əə..ɤoɛœ..ɜɞ..ʌɔææ..ɐɐ....aɶ..ä...ɑɒ';
+    this.diacritics = '..............................ll......ll...............................';
+    this.lower = "\u031E";
+  }
+  toChar(closedness, frontedness, rounded) {
+    var idx = this.idxsToInt(closedness, frontedness, rounded)
+    var ch = super.toChar(closedness, frontedness, rounded);
+    if(diacritics.charAt(idx) === 'l') {
+      return ch + this.lower;
+    } else {
+      return ch;
+    }
+  }
+}
+function init() { // called on page load
   console.log("init()");
   createBoxes(false);
 }
@@ -97,52 +174,12 @@ var extendd = 'iy..ɨʉ..ɯu..ɪʏ..ʊʊ..eø..ɘɵ..ɤoeø..əə..ɤoɛœ..ɜɞ
 var addlowr = '..............................ll......ll...............................';
 // var validcdhars = 'iyɨʉɯuɪʏʊʊeøɘɵɤoəəɛœɜɞʌɔææɐɐaɶɑɒ';
 var validchars = 'iyɨʉɯuɪʏʊeøɘɵɤoəɛœɜɞʌɔæɐaɶɑɒ';
-
-var simpleToChar = function(closedness, frontedness, rounded) {
-  if(rounded === 0.5) {
-    rounded = 0; // correct for 0.5 for both rounded/unrounded
-  }
-  var openidx = 6 - closedness * 2;
-  // turn the 0.5s into integers. Range: 0-3 -> 0-6 Len: 7
-  var backidx = 4 - frontedness * 2;
-  // turn the 0.5s into integers. Range: 0-2 -> 0-4 Len: 5
-  // Rounded is already ok.       Range: 0-1        Len: 2
-  var searchidx = rounded + backidx * 2 + openidx * 2 * 5;
-  // 2 comes from len(rounded);
-  // 10 comes from len(rounded x fronted) where x is cartesian product
-  // or 10 comes from len(rounded) * len(fronted) = 2 * 5
-  var ipachar = rfncstr.charAt(searchidx);
-  if(ipachar == '.') {
-    // i use this value to signify no result
-    console.log(`invalid char ${closedness},${frontedness},${rounded}`)
-  } else {
-    return ipachar;
-  }
+var charset = new IPACharsetBasic();
+function toChar(closedness, frontedness, rounded) {
+  return charset.toChar(closedness, frontedness, rounded);
 }
-
-var simpleCharToIdx = function(ipachar) {
-  var idxdouble = rfncstr.indexOf(ipachar + '' + ipachar);
-  // index of double char. This means that this char spans both
-  // the rounded and unrounded versions of itself. For example, ʊ shows up as
-  // ɪʏʊʊe because ʊ is both the rounded and unrounded version. If this is the
-  // case roundedness must be 0.5;
-  var rounded = -1;
-  if(idxdouble > -1) { // if there is a double version: if both r and unr
-    rounded = 0.5;
-  }
-  var idx = rfncstr.indexOf(ipachar);
-  if(idx > -1) {
-    if(rounded === -1) {
-      var rounded = idx % 2;
-    }
-    var backidx = parseInt(idx/2) % 5;
-    var openidx = parseInt(idx/10);
-    var closedness = (6 - openidx) / 2;
-    var frontedness = (4 - backidx) / 2;
-    return [closedness, frontedness, rounded];
-  } else {
-    console.log(`char not found ${char}`);
-  }
+function charToIdx(ipachar) {
+  return charset.charToIdx(ipachar);
 }
 function idxToElement(closedness, frontedness, rounded) {
   if(rounded === 0.5) {
@@ -165,15 +202,3 @@ function removeDup(str) {
 }
 var toChar = simpleToChar;
 var charToIdx = simpleCharToIdx;
-
-class IPACharset {
-  constructor() {}
-  toChar() {}
-  charToIdx() {}
-  idxToElement() {}
-}
-class IPACharsetBasic extends IPACharset {
-  toChar() {
-
-  }
-}
